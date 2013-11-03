00fdc6a341f52c2ea21713cdad9f98737ecccd23
Christian Vaas <christianvaas@auspex.eu>
2013-11-03 15:30:09 +0100

    Minor formatting

--- express-guess-lang.js
+++ express-guess-lang.js
@@ -1,16 +1,19 @@
 var egl = exports
 
 egl.init = function (request, response, next) {
-    var accept_lang = request.headers['accept-language'], regions = [], langs = [], prefered = { lang: 'en', likelihood: 0.0}
+    var accept_lang = request.headers['accept-language'],
+        regions = [],
+        langs = [],
+        prefered = { lang: 'en', likelihood: 0.0}
+
     var candidates = accept_lang.split(',')
     for (var i = 0; i < candidates.length; i++) {
-        var candidate = candidates[i]
-        var match = candidate.match(/q=([0-9].[0-9])/)
+        var match = candidates[i].match(/q=([0-9].[0-9])/)
         var likelihood = match ? match[1] : 0.0
-        match = candidate.match(/-([^;]*)/)
+        match = candidates[i].match(/-([^;]*)/)
         if (match)
             regions.push(match[1])
-        match = candidate.match(/(.*)[;||-]/)
+        match = candidates[i].match(/(.*)[;||-]/)
         var lang = match ? match[1] : ''
         var newLang = { lang: lang, likelihood: likelihood }
         if (newLang.likelihood > prefered.likelihood)
@@ -18,9 +21,11 @@ egl.init = function (request, response, next) {
         if (likelihood > 0.0)
             langs.push(newLang)
     }
+
     request.getRegions = function () { return regions }
     request.getLanguages = function () { return langs }
     request.getLikelyLanguage = function () { return prefered.lang }
+
     if (typeof next === 'function')
         next()
 }
