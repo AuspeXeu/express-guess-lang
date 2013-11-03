e5dd5581a6d13f835eccdd02dd88b4a1f33dbf7b
Christian Vaas <christianvaas@auspex.eu>
2013-11-03 13:03:00 +0100

    Renamed quality and make function pass to next middleware

--- express-guess-lang.js
+++ express-guess-lang.js
@@ -1,23 +1,26 @@
 var egl = exports
 
 egl.init = function (request, response, next) {
-    var accept_lang = request.headers['accept-language'], regions = [], langs = [], prefered = { lang: 'en', quality: 0.0}
+    var accept_lang = request.headers['accept-language'], regions = [], langs = [], prefered = { lang: 'en', likelihood: 0.0}
     var candidates = accept_lang.split(',')
     for (var i = 0; i < candidates.length; i++) {
         var candidate = candidates[i]
         var match = candidate.match(/q=([0-9].[0-9])/)
-        var quality = match ? match[1] : 0.0
-        match = candidate.match(/-(.*);/)
+        var likelihood = match ? match[1] : 0.0
+        match = candidate.match(/-([^;]*)/)
         if (match)
             regions.push(match[1])
         match = candidate.match(/(.*)[;||-]/)
         var lang = match ? match[1] : ''
-        var newLang = { lang: lang, quality: quality }
-        if (newLang.quality > prefered.quality)
+        var newLang = { lang: lang, likelihood: likelihood }
+        if (newLang.likelihood > prefered.likelihood)
             prefered = newLang
-        langs.push(newLang)
+        if (likelihood > 0.0)
+            langs.push(newLang)
     }
     request.getRegions = function () { return regions }
     request.getLanguages = function () { return langs }
-    request.getPreferedLanguage = function () { return prefered }
+    request.getLikelyLanguage = function () { return prefered.lang }
+    if (typeof next === 'function')
+        next()
 }
